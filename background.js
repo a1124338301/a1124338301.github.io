d21c39831a042354fdd9ad4e1eef6105c7737980
e <1124338301@qq.com>
2020-03-24 21:49:56 +0800

    上传

---
+++ background.js
@@ -0,0 +1,13 @@
+﻿// Launcher script for Chrome Apps in the Chrome Web Store.
+chrome.app.runtime.onLaunched.addListener(function() {
+	chrome.app.window.create("index.html", {
+		id: "mainWindow",
+		innerBounds: {
+			width: 780,
+			height: 520
+		},
+		minWidth: 780,
+		minHeight: 520,
+		resizable: false
+	});
+});
